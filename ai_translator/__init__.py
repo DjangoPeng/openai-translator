e25db01f0288fddbf82c79fb1bffd140feb501fb
DjangoPeng <pjt73651@gmail.com>
2023-05-06 16:40:47 +0800

    support table translation

--- ai_translator/__init__.py
+++
@@ -1,5 +0,0 @@
-from .environment import Environment
-from .argument_parser import ArgumentParser
-from .config_loader import ConfigLoader
-from .model import Model
-from .pdf_translator import PDFTranslator
